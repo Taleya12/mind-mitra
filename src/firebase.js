35eb219c56b463424d381a95ef3f4f2f211f938d
Taleya <taleyajnec@gmail.com>
2026-05-28 13:28:36 +0530

    fix firebase direct config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBkIX13PxykVnVBsX7HWhcN48acp9s8O-E",
+  authDomain: "mind-mitra-37f85.firebaseapp.com",
+  projectId: "mind-mitra-37f85",
+  storageBucket: "mind-mitra-37f85.firebasestorage.app",
+  messagingSenderId: "889960062685",
+  appId: "1:889960062685:web:098fd29ff9584ff4490534"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
